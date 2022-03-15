0a4448a5a489c8efe0d927895183e09d9874685a
Meta-HG <93179418+Meta-HG@users.noreply.github.com>
2022-03-14 20:29:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -174,7 +174,7 @@ if __name__ == '__main__':
     parser.add_argument('--update_step', type=int, help='task-level inner update steps', default=5)
     parser.add_argument('--update_step_test', type=int, help='update steps for finetunning', default=5)
     parser.add_argument('--batch_num', type=int, help='meta batch size', default=10)
-    parser.add_argument('--k_spt', type=int, help='k shot for support set', default=20)
+    parser.add_argument('--k_spt', type=int, help='k shot for support set', default=5)
     parser.add_argument('--k_qry', type=int, help='k shot for query set', default=250)
     parser.add_argument('--hidden', type=int, help='Number of hidden units', default=16)
     parser.add_argument('--kd', type=int, default=1, help='Use knowledge distillation')
